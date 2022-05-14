c1f4913c99f73e6c78849c910554ea1630712537
Krzys938 <krzys938@gmail.com>
2022-05-14 19:41:55 +0200

    Dodanie 1 wersji dialogów

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    public Image image;
+
+    private Queue<string> sentences;
+
+    private void Awake()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+        image.sprite = dialogue.image;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    private void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
